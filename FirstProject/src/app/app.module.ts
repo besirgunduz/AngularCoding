9b933071d3c2bfb97688e7fdf05deec9042a4c14
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-06-16 23:42:34 +0300

    commit

--- FirstProject/src/app/app.module.ts
+++ FirstProject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponent } from './my/my.component';
+import { MypipePipe } from './mypipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent,
+    MypipePipe
   ],
   imports: [
     BrowserModule,
