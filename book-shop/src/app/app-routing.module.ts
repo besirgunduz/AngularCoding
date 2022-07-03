024b4319d33d5c0853bcc4709f95b13e8c131e8d
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-07-03 09:11:38 +0300

    Routing yapısı oluşturuldu

--- book-shop/src/app/app-routing.module.ts
+++ book-shop/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './ui/components/home/home.component';
+import { LayoutComponent } from './ui/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      { path: '', component: HomeComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- book-shop/src/app/ui/ui.module.ts
+++ book-shop/src/app/ui/ui.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout/layout.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { SidenavComponent } from './layout/sidenav/sidenav.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { LayoutModule } from './layout/layout.module';
+import { ComponentsModule } from './components/components.module';
 
 
 
 @NgModule({
-  declarations: [LayoutComponent, NavbarComponent, SidenavComponent, FooterComponent],
+  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule,
+    ComponentsModule
   ]
 })
 export class UiModule { }
