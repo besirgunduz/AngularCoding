3d905b3d353044beab9694f6467bfa6a1f7c1f79
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-07-05 21:02:26 +0300

    Spinner and toastr

--- book-shop/src/app/app.module.ts
+++ book-shop/src/app/app.module.ts
@@ -5,12 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxSpinnerModule } from "ngx-spinner";
+
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, UiModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UiModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      progressBar: true
+    })
+  ],
   providers: [
     { provide: 'apiUrl', useValue: 'https://webapi.angulareducation.com/api/' },
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
+
+
--- book-shop/src/app/ui/login/service/auth.service.ts
+++ book-shop/src/app/ui/login/service/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 import { LoginModel } from '../models/loginModel';
 
 @Injectable({
@@ -10,17 +12,29 @@ export class AuthService {
   constructor(
     @Inject('apiUrl') private apiUrl: string,
     private httpClient: HttpClient,
-    private router: Router
+    private router: Router,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService
   ) { }
 
   login(loginModel: LoginModel) {
+    this.spinner.show();
     let api = this.apiUrl + 'users/login';
     this.httpClient.post(api, loginModel).subscribe(
       (response: any) => {
+        this.spinner.hide();
         localStorage.setItem('token', response.data.token);
+        this.toastr.success("Giriş işlemi başarılı");
         this.router.navigate(["/"])
       },
-      (error) => { }
+      (error) => {
+        this.spinner.hide();
+      }
     );
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(["/login"]);
   }
 }
